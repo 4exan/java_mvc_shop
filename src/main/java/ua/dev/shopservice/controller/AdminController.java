3ed251a1961a520f30471e9c646135391ef7dcf5
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-06 15:34:24 +0300

    add: AdminController

---
+++ src/main/java/ua/dev/shopservice/controller/AdminController.java
@@ -0,0 +1,30 @@
+package ua.dev.shopservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ua.dev.shopservice.repository.ItemRepository;
+import ua.dev.shopservice.repository.UserRepository;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public AdminController(UserRepository userRepository, ItemRepository itemRepository){
+        this.userRepository = userRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    public String adminDashboard(Model model){
+        model.addAttribute("items", itemRepository.findAll());
+        model.addAttribute("users", userRepository.findAll());
+        return "admin/dashboard";
+    }
+    
+}
