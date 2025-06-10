eded3747ea6bd792a1c16970b0071dfd2adc95ce
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-10 14:16:09 +0300

    fix(SecurityConfig): Fixed authorization error

--- src/main/java/ua/dev/shopservice/config/SecurityConfig.java
+++ src/main/java/ua/dev/shopservice/config/SecurityConfig.java
@@ -9,12 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import ua.dev.shopservice.repository.UserRepository;
@@ -25,62 +21,62 @@ import ua.dev.shopservice.service.UserDetailsServiceImpl;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final UserDetailsServiceImpl userDetailsServiceImpl;
+  private final UserDetailsServiceImpl userDetailsServiceImpl;
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    SecurityConfig(UserRepository userRepository, UserDetailsServiceImpl userDetailsServiceImpl) {
-        this.userRepository = userRepository;
-        this.userDetailsServiceImpl = userDetailsServiceImpl;
-    }
-    
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
+  SecurityConfig(UserRepository userRepository, UserDetailsServiceImpl userDetailsServiceImpl) {
+    this.userRepository = userRepository;
+    this.userDetailsServiceImpl = userDetailsServiceImpl;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/h2-console/**").permitAll()
             .requestMatchers("/shop/**", "/").permitAll()
-            .requestMatchers("/admin/**").authenticated())
+            .requestMatchers("/admin/**").authenticated()
+            .requestMatchers("/css/**", "/js/**").permitAll())
         .csrf(csrf -> csrf
             .ignoringRequestMatchers("/h2-console/**"))
         .headers((headers) -> headers
             .defaultsDisabled()
-            .frameOptions(frameOptions -> frameOptions.sameOrigin())
-            )
+            .frameOptions(frameOptions -> frameOptions.sameOrigin()))
         .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
         .logout(logout -> logout
             .logoutSuccessUrl("/shop")
-            .permitAll()
-        );
-        return http.build();
-    }
+            .permitAll());
+    return http.build();
+  }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsServiceImpl);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
-    }
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsServiceImpl);
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public CommandLineRunner run(){
-        return args -> {
-            if(userRepository.findByEmail("admin").isEmpty()){
-                ua.dev.shopservice.model.User user = new ua.dev.shopservice.model.User();
-                user.setEmail("admin");
-                user.setPassword(passwordEncoder().encode("admin"));
-                user.setRole("ADMIN");
-                userRepository.save(user);
-            }else{
-                System.out.println("Loaded user: " + userRepository.findByEmail("admin").orElseThrow(()->new RuntimeException()).toString());
-            }
-        };
-    }
+  @Bean
+  public CommandLineRunner run() {
+    return args -> {
+      if (userRepository.findByEmail("admin").isEmpty()) {
+        ua.dev.shopservice.model.User user = new ua.dev.shopservice.model.User();
+        user.setEmail("admin");
+        user.setPassword(passwordEncoder().encode("admin"));
+        user.setRole("ADMIN");
+        userRepository.save(user);
+      } else {
+        System.out.println(
+            "Loaded user: " + userRepository.findByEmail("admin").orElseThrow(() -> new RuntimeException()).toString());
+      }
+    };
+  }
 
 }
