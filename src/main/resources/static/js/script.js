c08a6572cec62f1450d5a3fea557008f6522fe6e
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-11 18:43:33 +0300

    feat: add user edit modal to template and script

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const editItemModal = document.getElementById("editItemModal");
+  const editUserModal = document.getElementById("editUserModal");
 
   document.querySelectorAll(".editItemBtn").forEach((button) => {
 
@@ -20,12 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("quantity").value = button.dataset.quantity;
       document.getElementById("available").checked = button.dataset.available === "true";
       document.getElementById("sale").checked = button.dataset.sale === "true";
-
       editItemModal.style.display = "block";
     });
   });
 
+  document.querySelectorAll(".editUserBtn").forEach((button) => {
+    button.addEventListener('click', () => {
+      document.getElementById('userId').value = button.dataset.id;
+      document.getElementById('firstname').value = button.dataset.firstname;
+      document.getElementById('lastname').value = button.dataset.lastname;
+      document.getElementById('email').value = button.dataset.email;
+      document.getElementById('phone').value = button.dataset.phone;
+      document.getElementById('role').value = button.dataset.role;
+      editUserModal.style.display = "block";
+    })
+  })
+
   document.querySelectorAll(".closeItemModalBtn").forEach(el => {
+    el.addEventListener('click', () => {
+      editItemModal.style.display = "none";
+    })
+  })
+
+  document.querySelectorAll(".closeUserModalBtn").forEach(el => {
     el.addEventListener('click', () => {
       editItemModal.style.display = "none";
     })
@@ -36,5 +54,11 @@ document.addEventListener("DOMContentLoaded", () => {
       editItemModal.style.display = "none";
     }
   };
+
+  window.onclick = function (event){
+    if(event.target === editUserModal){
+      editUserModal.style.display = "none";
+    }
+  }
 })
 
